33703a6176089c515bfbf93a46403849ce639bf9
arslan-ashraf <arslanash@gmail.com>
2024-12-21 16:39:14 -0500

    changes

--- house-robber.cpp
+++ house-robber.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-int backtrack(vector<int> &nums, int start_level){
-	if (start_level >= nums.size()) return 0;
-	int rob_current = nums[start_level] + backtrack(nums, start_level + 2);
-	int rob_next = backtrack(nums, start_level + 1);
+int backtrack(vector<int> &nums, int current_level){
+	if (current_level >= nums.size()) return 0;
+	int rob_current = nums[current_level] + backtrack(nums, current_level + 2);
+	int rob_next = backtrack(nums, current_level + 1);
 	return max(rob_current, rob_next);
 }
 
 int house_robber(vector<int> &nums){
-	int start_level = 0;
-	int result = backtrack(nums, start_level);
+	int current_level = 0;
+	int result = backtrack(nums, current_level);
 	return result;
 }
 
@@ -38,21 +38,26 @@ int main(){
 
 using namespace std;
 
-int backtrack(vector<int> &nums, int memo[], int start_level){
-	if (start_level >= nums.size()) return 0;
-	if (memo[start_level] >= 0) return memo[start_level];
-	int rob_current = nums[start_level] + backtrack(nums, memo, start_level + 2);
-	int rob_next = backtrack(nums, memo, start_level + 1);
+int backtrack(vector<int> &nums, int memo[], int current_level){
+	if (current_level >= nums.size()) return 0;
+	if (memo[current_level] >= 0) return memo[current_level];
+
+	// rob current and move two levels down
+	int rob_current = nums[current_level] + backtrack(nums, memo, current_level + 2);
+
+	// don't rob the current and move only one level down
+	int rob_next = backtrack(nums, memo, current_level + 1);
 	int max_gain = max(rob_current, rob_next);
-	memo[start_level] = max_gain;
+
+	memo[current_level] = max_gain;
 	return max_gain;
 }
 
 int house_robber(vector<int> &nums){
 	int memo[nums.size()];
 	std::fill(memo, memo + nums.size(), -1); // fill is from <algorithm>
-	int start_level = 0;
-	int result = backtrack(nums, memo, start_level);
+	int current_level = 0;
+	int result = backtrack(nums, memo, current_level);
 	return result;
 }
 
