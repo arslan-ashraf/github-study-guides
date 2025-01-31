1348912c8b3f575e5e785b48bd3a3009c380466d
arslan-ashraf <arslanash@gmail.com>
2025-01-31 17:12:38 -0500

    changes

---
+++ jump-game-I.js
@@ -0,0 +1,19 @@
+function jump_game_I(arr){
+	let count = 1
+	let destination = arr.length - 1
+	for(let i = arr.length - 2; i >= 0; i--){
+		if (arr[destination - count] + i >= destination){
+			destination = destination - count
+			count = 1
+		} else {
+			count += 1
+		}
+	}
+	return destination == 0 ? true : false
+}
+
+jump_game_I([2, 4, 0, 1, 0, 1, 4]) // true
+jump_game_I([2, 3, 1, 1, 4]) // true
+jump_game_I([3, 2, 1, 0, 4]) // false
+jump_game_I([6, 4, 3, 1, 0, 1, 0, 4]) // false
+jump_game_I([7, 4, 3, 1, 0, 1, 0, 4]) // true
