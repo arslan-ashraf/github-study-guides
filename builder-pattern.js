b2a40da77ee5cf3403ed854abc5b9a7c5c586cc9
arslan-ashraf <arslanash@gmail.com>
2025-03-28 15:43:00 -0400

    changes

--- builder-pattern.js
+++ builder-pattern.js
@@ -1,96 +1,37 @@
-// Product class (the complex object to be built)
-class Product {
-    constructor() {
-        this.parts = []
-    }
+class User {
+	constructor(name) {
+		this.name = name
+		this.age = null
+		this.phone = null
+		this.address = null
+	}
 
-    add_part(part) {
-        this.parts.push(part)
-    }
+	set_age(age) {
+		this.age = age
+		return this
+	}
 
-    show_parts() {
-        console.log("Product Parts: ", this.parts.join(', '))
-    }
+	set_phone(phone) {
+		this.phone = phone
+		return this
+	}
+
+	set_address(address) {
+		this.address = address
+		return this
+	}
+
+	build() {
+		return this
+	}
 }
 
-// Abstract Builder class
-class Builder {
-    build_part1() {}
-    build_part2() {}
-    build_part3() {}
-    get_result() {}
-}
 
-// Concrete Builder 1
-class ConcreteBuilder1 extends Builder {
-    constructor() {
-        super()
-        this.product = new Product()
-    }
+// Usage
+const user1 = new User("abc").set_age(30).set_phone("123-456-7890").build()
+const user2 = new User("def").set_address("123 Main St").build()
+const user3 = new User("ghi").set_age(25).build()
 
-    build_part1() {
-        this.product.add_part("Part 1 from Builder 1")
-    }
-
-    build_part2() {
-        this.product.add_part("Part 2 from Builder 1")
-    }
-
-    build_part3() {
-        this.product.add_part("Part 3 from Builder 1")
-    }
-
-    get_result() {
-        return this.product
-    }
-}
-
-// Concrete Builder 2
-class ConcreteBuilder2 extends Builder {
-    constructor() {
-        super()
-        this.product = new Product()
-    }
-
-    build_part1() {
-        this.product.add_part("Part 1 from Builder 2")
-    }
-
-    build_part2() {
-        this.product.add_part("Part 2 from Builder 2")
-    }
-
-    build_part3() {
-        this.product.add_part("Part 3 from Builder 2")
-    }
-
-    get_result() {
-        return this.product
-    }
-}
-
-// Director class (directs the building process)
-class Director {
-    constructor(builder) {
-        this.builder = builder
-    }
-
-    construct() {
-        this.builder.build_part1()
-        this.builder.build_part2()
-        this.builder.build_part3()
-    }
-}
-
-// Client code
-const builder1 = new ConcreteBuilder1()
-const director1 = new Director(builder1)
-director1.construct()
-const product1 = builder1.get_result()
-product1.show_parts() // Output: Product Parts: Part 1 from Builder 1, Part 2 from Builder 1, Part 3 from Builder 1
-
-const builder2 = new ConcreteBuilder2()
-const director2 = new Director(builder2)
-director2.construct()
-const product2 = builder2.get_result()
-product2.show_parts() // Output: Product Parts: Part 1 from Builder 2, Part 2 from Builder 2, Part 3 from Builder 2
+console.log(user1)
+console.log(user2)
+console.log(user3)
