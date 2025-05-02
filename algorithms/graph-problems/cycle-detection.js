54569c6a98e4420739a6fc6456300e4339438dc3
arslan-ashraf <arslanash@gmail.com>
2025-05-02 16:16:08 -0400

    changes

--- algorithms/graph-problems/cycle-detection.js
+++ algorithms/graph-problems/cycle-detection.js
@@ -2,7 +2,7 @@ function detect_cycle_in_graph(graph){
 	
 }
 
-let graph = {
+let cyclic_graph = {
 	"A": { "B": 3, "D": 1, "F": 5},
 	"B": { "C": 4},
 	"C": { "E": 3 },
@@ -11,7 +11,7 @@ let graph = {
 	"F": { "M": 1 },
 	"G": { "H": 2, "I": 1 },
 	"H": { "I": 3, "K": 4 },
-	"I": { "J": 4, "L": 2 },
+	"I": { "E": 4, "J": 4, "L": 2 },
 	"J": {},
 	"K": {},
 	"L": {},
