113acf8372eca6508278c8d41b6d99c0fa0db104
arslan-ashraf <arslanash@gmail.com>
2025-06-21 12:44:09 -0400

    changes

---
+++ algorithms/sorting-problems/group-anagrams.js
@@ -0,0 +1,17 @@
+function group_anagrams(words_array){
+	let hash_map = new Map()
+	let result = []
+	words_array.map((word) => {
+		let word_sorted = word.split("").sort().join("")
+		if (hash_map[word_sorted]){
+			hash_map[word_sorted].push(word)
+		} else {
+			hash_map[word_sorted] = [word]
+		}
+	})
+
+	for (let key in hash_map){
+		result.push(hash_map[key])
+	}
+	return result
+}
