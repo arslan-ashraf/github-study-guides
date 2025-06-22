f87a71f409891711385ea3e61620b8d533caad1f
arslan-ashraf <arslanash@gmail.com>
2025-06-21 22:30:21 -0400

    changes

--- algorithms/graph-problems/jump-game-I.js
+++ algorithms/graph-problems/jump-game-I.js
@@ -17,3 +17,15 @@ jump_game_I([2, 3, 1, 1, 4]) // true
 jump_game_I([3, 2, 1, 0, 4]) // false
 jump_game_I([6, 4, 3, 1, 0, 1, 0, 4]) // false
 jump_game_I([7, 4, 3, 1, 0, 1, 0, 4]) // true
+
+
+// a simpler more intuitive solution
+function jump_game(arr){
+	let destination = arr.length - 1
+	for (let current_index = arr.length - 2; current_index >= 0; current_index--){
+		if (current_index + arr[current_index] >= destination){
+			destination = current_index
+		}
+	}
+	return destination == 0 ? true : false
+}
