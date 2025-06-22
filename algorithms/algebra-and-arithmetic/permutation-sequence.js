eedd255160bae048afa2c1b3dfcd81fc192d18af
arslan-ashraf <arslanash@gmail.com>
2025-06-21 23:14:17 -0400

    changes

--- algorithms/algebra-and-arithmetic/permutation-sequence.js
+++ algorithms/algebra-and-arithmetic/permutation-sequence.js
@@ -1,13 +1,21 @@
 function permutation_sequence(n, k){
-	let numbers_used = new Array(n + 1).fill(false)
-	let result = ""
-	while (k > 0){
-		let n_minus_one_factorial = factorial(n - 1)
-		let current_number = Math.floor(k / n_minus_one_factorial) + 1
-		result += String(current_number)
-		k = k % n_minus_one_factorial
-		n -= 1
+	let range_for_numbers = new Array(n + 1)
+	let n_minus_one_factorial = factorial(n - 1)
+	let range_start = 1
+	let end_range = n_minus_one_factorial
+	for (let i = 1; i <= n; i++){
+		range_for_numbers[i] = [range_start, end_range]
+		range_start = end_range + 1
+		end_range += n_minus_one_factorial
 	}
+	console.log(range_for_numbers)
+	// let result = ""
+	// while (k > 0){
+	// 	let current_number = Math.floor(k / n_minus_one_factorial) + 1
+	// 	result += String(current_number)
+	// 	k = k % n_minus_one_factorial
+	// 	n -= 1
+	// }
 }
 
 function factorial(x){
@@ -17,3 +25,6 @@ function factorial(x){
 	}
 	return result
 }
+
+
+console.log(permutation_sequence(4))
