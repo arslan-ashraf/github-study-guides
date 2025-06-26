1d0cc67f91a0013ba038434a6a5d4b3a70032ac2
arslan-ashraf <arslanash@gmail.com>
2025-06-26 11:20:33 -0400

    changes

--- algorithms/algebra-and-arithmetic/permutation-sequence.js
+++ algorithms/algebra-and-arithmetic/permutation-sequence.js
@@ -1,21 +1,27 @@
 function permutation_sequence(n, k){
-	let range_for_numbers = new Array(n + 1)
-	let n_minus_one_factorial = factorial(n - 1)
-	let range_start = 1
-	let end_range = n_minus_one_factorial
-	for (let i = 1; i <= n; i++){
-		range_for_numbers[i] = [range_start, end_range]
-		range_start = end_range + 1
-		end_range += n_minus_one_factorial
+	let numbers_used = new Array(n + 1).fill(false)
+	let result = ""
+	while (k > 1){
+		let n_minus_one_factorial = factorial(n - 1)
+		let permutation_group = Math.ceil(k / n_minus_one_factorial)
+		let number = permutation_group_first_number(permutation_group, numbers_used)
+		numbers_used[number] = true
+		result += String(number)
+		k = k % n_minus_one_factorial
+		n -= 1
 	}
-	console.log(range_for_numbers)
-	// let result = ""
-	// while (k > 0){
-	// 	let current_number = Math.floor(k / n_minus_one_factorial) + 1
-	// 	result += String(current_number)
-	// 	k = k % n_minus_one_factorial
-	// 	n -= 1
-	// }
+	console.log(numbers_used)
+	if (k == 1){
+		for (let i = 1; i < numbers_used.length; i++){
+			if (numbers_used[i] == false) result += String(i)
+		}
+	}
+	if (k == 0){
+		for (let i = numbers_used.length - 1; i >= 1; i--){
+			if (numbers_used[i] == false) result += String(i)
+		}
+	}
+	return result
 }
 
 function factorial(x){
@@ -26,5 +32,15 @@ function factorial(x){
 	return result
 }
 
+function permutation_group_first_number(permutation_group, numbers_used){
+	for(let i = permutation_group; i < numbers_used.length; i++){
+		if (numbers_used[i] == false) return i
+	}
+}
 
-console.log(permutation_sequence(4))
+
+console.log(permutation_sequence(4, 1))
+console.log(permutation_sequence(4, 2))
+console.log(permutation_sequence(4, 6))
+console.log(permutation_sequence(4, 7))
+console.log(permutation_sequence(4, 3))
