1770a1fb76ef726c70155a81c9478def372fc9fa
arslan-ashraf <arslanash@gmail.com>
2025-06-27 16:17:54 -0400

    changes

--- algorithms/array-matrix/set-matrix-zeros.js
+++ algorithms/array-matrix/set-matrix-zeros.js
@@ -12,6 +12,9 @@ function set_matrix_zeros_1(matrix){
 		}
 	}
 
+	// console.log(row_zeros)
+	// console.log(column_zeros)
+
 	for (let i = 0; i < row_zeros.length; i++){
 		if (row_zeros[i] == true){
 			for (let j = 0; j < matrix[i].length; j++){
@@ -31,6 +34,46 @@ function set_matrix_zeros_1(matrix){
 	return matrix
 }
 
+let matrix1 = [
+	[1, 3, 1, 8, 5, 6],
+	[5, 2, 9, 4, 0, 6],
+	[6, 7, 7, 3, 5, 1],
+	[4, 0, 5, 3, 1, 9],
+	[1, 6, 1, 9, 5, 8]
+]
+
+console.log(set_matrix_zeros_1(matrix1))
+
+
+function set_matrix_zeros(matrix){
+	for(let i = 0; i < matrix.length; i++){
+		for (let j = 0; j < matrix[i].length; j++){
+			if (matrix[i][j] == 0){
+				matrix[0][j] = 0
+				matrix[i][0] = 0
+			}
+		}
+	}
+
+	for (let i = 0; i < matrix.length; i++){
+		if (matrix[i][0] == 0){
+			for (let j = 0; j < matrix[i].length; j++){
+				matrix[i][j] = 0
+			}
+		}
+	}
+
+	for (let j = 0; j < matrix[0].length; j++){
+		if (matrix[0][j] == 0){	
+			for(let i = 0; i < matrix.length; i++){
+				matrix[i][j] = 0		
+			}
+		}
+	}
+
+	return matrix
+}
+
 let matrix = [
 	[1, 3, 1, 8, 5, 6],
 	[5, 2, 9, 4, 0, 6],
@@ -39,4 +82,4 @@ let matrix = [
 	[1, 6, 1, 9, 5, 8]
 ]
 
-console.log(set_matrix_zeros_1(matrix))
+console.log(set_matrix_zeros(matrix))
