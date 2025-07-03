98567f8f1ce33e3e090b828ddac5bacc03ae6870
arslan-ashraf <arslanash@gmail.com>
2025-07-03 14:20:35 -0400

    changes

--- algorithms/backtracking/word-break-ii.js
+++ algorithms/backtracking/word-break-ii.js
@@ -40,6 +40,7 @@ console.log(
 
 
 function word_break_ii_memoized(str, words){
+	// for the memoized solution, result is redundant, we can just return memo[0]
 	let result = []
 	let left_index = 0
 	let memo = {}
@@ -54,6 +55,8 @@ function word_break_ii_memoized(str, words){
 
 function dfs_memoized(str, word_set, left_index, result, temp_str, memo){
 	if (memo[left_index] != undefined){
+		// the loop is needed to combine all of the branches from the
+		// current node (temp_str) to memoized nodes
 		for(let i = 0; i < memo[left_index].length; i++){
 			result.push((temp_str + " " + memo[left_index][i]).trim())
 		}
@@ -68,6 +71,8 @@ function dfs_memoized(str, word_set, left_index, result, temp_str, memo){
 			// implicit right_substring with right_index + 1
 			dfs_memoized(str, word_set, right_index + 1, result, temp_str + " " + left_substring, memo)
 			
+			// check if nodes further down the tree have already been memoized
+			// if so, combine current string (left_substring) with previously memoized nodes
 			if (memo[right_index + 1] != undefined){
 
 				for (let i = 0; i < memo[right_index + 1].length; i++){
@@ -78,7 +83,7 @@ function dfs_memoized(str, word_set, left_index, result, temp_str, memo){
 					}
 				}
 				
-			} else {
+			} else { // if not, memoize the bottom most leaf nodes
 
 				if (memo[left_index] != undefined){
 					memo[left_index].push(left_substring + " " + str.substring(right_index + 1))
@@ -94,14 +99,14 @@ function dfs_memoized(str, word_set, left_index, result, temp_str, memo){
 
 
 // the memo map should be
-// memo = { 
-// 	0:  ['down load sand work shop', 'down load sand workshop ', 'down loads and work shop', 'down loads and workshop ', 'download sand work shop', 'download sand workshop ', 'downloads and work shop', 'downloads and workshop '],
-// 	4:  ['load sand work shop', 'load sand workshop ', 'loads and work shop', 'loads and workshop '],
-// 	8:  ['sand work shop', 'sand workshop '],
-// 	9:  ['and work shop', 'and workshop '],
-// 	12: ['work shop', 'workshop '],
-// 	16: ['shop']
-// }
+let _memo = { 
+	0:  ['down load sand work shop', 'down load sand workshop ', 'down loads and work shop', 'down loads and workshop ', 'download sand work shop', 'download sand workshop ', 'downloads and work shop', 'downloads and workshop '],
+	4:  ['load sand work shop', 'load sand workshop ', 'loads and work shop', 'loads and workshop '],
+	8:  ['sand work shop', 'sand workshop '],
+	9:  ['and work shop', 'and workshop '],
+	12: ['work shop', 'workshop '],
+	16: ['shop']
+}
 
 // should return: [
 // 		"down load sand work shop", "down load sand workshop",
