eb9901a1713c90c8eaeb8d997b7695097b7c0fd7
arslan-ashraf <arslanash@gmail.com>
2025-07-03 12:08:16 -0400

    changes

--- algorithms/backtracking/word-break-ii.js
+++ algorithms/backtracking/word-break-ii.js
@@ -11,13 +11,12 @@ function word_break_ii(str, words){
 
 function dfs(str, word_set, left_index, result, temp_str){
 	if (left_index == str.length){
-		result.push(temp_str)
+		result.push(temp_str.trim())
 	}
-	for (let right_index = left_index + 1; right_index < str.length; right_index++){
-		let left_substring = str.substring(left_index, right_index)
+	for (let right_index = left_index; right_index < str.length; right_index++){
+		let left_substring = str.substring(left_index, right_index + 1)
 		if (word_set.has(left_substring)){
-			let right_substring = dfs(str, word_set, right_index, result, temp_str + left_substring)
-
+			dfs(str, word_set, right_index + 1, result, temp_str + " " + left_substring)
 		}
 	}
 }
@@ -25,11 +24,13 @@ function dfs(str, word_set, left_index, result, temp_str){
 
 // should return: [
 // 		"down load sand work shop", "down load sand workshop",
-// 		"dow"
+// 		"down loads and work shop", "down loads and workshop",
+// 		"download sand work shop", "download sand workshop",
+// 		"downloads and work shop", "downloads and workshop"
 // ]
 console.log(
 	word_break_ii(
 		"downloadsandworkshop", 
-		["down", "download", "downloads", "and", "sand", "work", "workshop"]
+		["down", "load", "loads", "download", "downloads", "and", "sand", "work", "shop", "workshop"]
 	)
 )
