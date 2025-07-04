aa57c32dfde63c38721e04e5021c743399cbe0d0
arslan-ashraf <arslanash@gmail.com>
2025-07-04 16:37:19 -0400

    changes

--- algorithms/linked-lists/partition-linked-list.js
+++ algorithms/linked-lists/partition-linked-list.js
@@ -2,20 +2,22 @@ function partition_linked_list(head_node, x){
 	let left_head_node = new ListNode()
 	let right_head_node = new ListNode()
 
-	let left_tail_node = left_head_node
-	let right_tail_node = right_head_node
+	let left_node = left_head_node
+	let right_node = right_head_node
 
 	while(head_node != null){
 		if (head_node.val < x){
-			left_tail_node.next = head_node
-			left_tail_node = left_tail_node.next
+			left_node.next = head_node
+			left_node = left_node.next
 		} else {
-			right_tail_node.next = head_node
-			right_tail_node = right_tail_node.next
+			right_node.next = head_node
+			right_node = right_node.next
 		}
 		head_node = head_node.next
 	}
-	left_tail_node.next = right_head_node
-	right_tail_node.next = null
+
+	left_node.next = right_head_node
+	right_node.next = null
+	
 	return left_head_node.next
 }
