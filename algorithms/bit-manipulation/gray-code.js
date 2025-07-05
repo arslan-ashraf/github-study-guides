3bc767e82c49b7f86440360873bbc77ce117c838
arslan-ashraf <arslanash@gmail.com>
2025-07-05 11:38:26 -0400

    changes

--- algorithms/bit-manipulation/gray-code.js
+++ algorithms/bit-manipulation/gray-code.js
@@ -9,3 +9,34 @@ function gray_code(n){
 
 console.log(gray_code(2))
 console.log(gray_code(3))
+
+
+function gray_code_induction(n){
+	let gray_codes = get_gray_codes_recursive(n)
+	console.log(gray_codes)
+	for (let i = 0; i< gray_codes.length; i++){
+		gray_codes[i] = parseInt(gray_codes[i], 2)
+	}
+	return gray_codes
+}
+
+function get_gray_codes_recursive(n){
+	if (n <= 1) return ["0", "1"]
+
+	let gray_codes_n_minus_1 = get_gray_codes_recursive(n - 1)
+
+	let result = []
+
+	for (let i = 0; i < gray_codes_n_minus_1.length; i++){
+		result.push("0" + gray_codes_n_minus_1[i])
+	}
+
+	for (let i = gray_codes_n_minus_1.length - 1; i >= 0; i--){
+		result.push("1" + gray_codes_n_minus_1[i])
+	}
+
+	return result
+}
+
+console.log(gray_code_induction(2))
+console.log(gray_code_induction(3))
