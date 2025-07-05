22480b1374a5bcafb05a8c401375b0559bf6b870
arslan-ashraf <arslanash@gmail.com>
2025-07-05 10:32:06 -0400

    changes

--- algorithms/bit-manipulation/gray-code.js
+++ algorithms/bit-manipulation/gray-code.js
@@ -1,8 +1,8 @@
 function gray_code(n){
 	let gray_codes = []
-	let total_codes = 1 << n
-	for (let i = 0; i < total_codes; i++){
-		gray_codes.push(i ^ (i >> 1))
+	let total_codes = 1 << n 		// ex: if n = 3, 1 << 3 = 8
+	for (let k = 0; k < total_codes; k++){
+		gray_codes.push(k ^ (k >> 1))
 	}
 	return gray_codes
 }
