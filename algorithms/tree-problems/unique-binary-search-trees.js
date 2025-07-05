0c80c3de161ac0e076d173b6c8205dd938f2b910
arslan-ashraf <arslanash@gmail.com>
2025-07-04 22:58:12 -0400

    changes

--- algorithms/tree-problems/unique-binary-search-trees.js
+++ algorithms/tree-problems/unique-binary-search-trees.js
@@ -15,3 +15,24 @@ function dfs(n){
 console.log(unique_binary_search_trees(3) == 5)
 console.log(unique_binary_search_trees(4) == 14)
 console.log(unique_binary_search_trees(5) == 42)
+
+
+function unique_binary_search_trees_memoized(n){
+	let memo = {}
+	return dfs_memoized(n, memo)
+}
+
+function dfs_memoized(n, memo){
+	console.log(memo)
+	if (n <= 1) return 1
+	if (memo[n] != undefined) return memo[n]
+
+	let result = 0
+	for (let i = 1; i <= n; i++){
+		result += dfs_memoized(i - 1, memo) * dfs_memoized(n - i, memo)
+	}
+	memo[n] = result
+	return result
+}
+
+console.log(unique_binary_search_trees_memoized(5) == 42)
