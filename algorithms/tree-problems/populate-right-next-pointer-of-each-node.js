331b94dbecc1cffa8d80466497120f5b1b9edee2
arslan-ashraf <arslanash@gmail.com>
2025-07-12 17:42:00 -0400

    changes

--- algorithms/tree-problems/populate-right-next-pointer-of-each-node.js
+++ algorithms/tree-problems/populate-right-next-pointer-of-each-node.js
@@ -6,7 +6,6 @@ function TreeNode(value){
 }
 
 function populate_right_next_pointer_of_each_node_DFS(root_node){
-	
 	preorder_dfs(root_node)
 }
 
@@ -23,4 +22,31 @@ function preorder_dfs(current_tree_node){
 
 	preorder_dfs(current_tree_node.left)
 	preorder_dfs(current_tree_node.right)
+}
+
+
+function populate_right_next_pointer_of_each_node_BFS_no_queue(root_node){
+	let dummy_root_pointer = root_node
+	let current_tree_node = root_node
+
+	while (current_tree_node != null){
+
+		let current_level_current_node = current_tree_node
+		while (current_level_current_node != null){
+
+			if (current_level_current_node.left){  // same as DFS solution
+				current_level_current_node.left.next = current_level_current_node.right
+
+				if (current_level_current_node.next){
+					current_level_current_node.right.next = current_level_current_node.next.left
+				}
+			} else {
+				break
+			}
+
+			current_level_current_node = current_level_current_node.next
+		}
+
+		current_tree_node = current_tree_node.left
+	}
 }
