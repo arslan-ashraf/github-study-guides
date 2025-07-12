9977546e061629095f063ac498f988dac91d31f7
arslan-ashraf <arslanash@gmail.com>
2025-07-12 13:36:33 -0400

    changes

--- algorithms/tree-problems/flatten-binary-tree-to-linked-list-preorder.js
+++ algorithms/tree-problems/flatten-binary-tree-to-linked-list-preorder.js
@@ -4,7 +4,23 @@ function TreeNode(value){
 	this.right = null
 }
 
+let previous_node = null
 
+function flatten_binary_tree_to_linked_list_preorder(root_node){
+	flatten(root_node)
+}
+
+function flatten(tree_node){
+	if (tree_node == null) return
+
+	flatten(tree_node.right)
+	flatten(tree_node.left)
+
+	tree_node.right = previous_node
+	tree_node.left = null
+
+	previous_node = tree_node
+}
 
 
 let one = new TreeNode(1)
@@ -24,3 +40,5 @@ two.right = four
 
 five.left = six
 five.right = seven
+
+flatten_binary_tree_to_linked_list_preorder(one)
