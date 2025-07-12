2595e4c1062a0b98f0a91057ca648e9cdb27a5ea
arslan-ashraf <arslanash@gmail.com>
2025-07-12 19:51:02 -0400

    changes

--- algorithms/tree-problems/populate-right-next-pointer-of-each-node-ii.js
+++ algorithms/tree-problems/populate-right-next-pointer-of-each-node-ii.js
@@ -6,9 +6,10 @@ function TreeNode(value){
 }
 
 function populate_right_next_pointer_of_each_node_ii_BFS(root_node){
-	let dummy_node = new TreeNode(0)
-	let linked_list_tail_node = dummy_node
 	let current_node = root_node
+
+	let linked_list_head_node = new TreeNode(0)
+	let linked_list_tail_node = linked_list_head_node
 
 	while (current_node != null){
 		if (current_node.left != null){
@@ -21,9 +22,9 @@ function populate_right_next_pointer_of_each_node_ii_BFS(root_node){
 		}
 		current_node = current_node.next
 		if (current_node == null){
-			linked_list_tail_node = dummy_node
-			current_node = dummy_node.next   // move down one level to left most node
-			dummy_node.next = null
+			linked_list_tail_node = linked_list_head_node // reset tail node to new TreeNode(0)
+			current_node = linked_list_head_node.next     // move down one level to left most node
+			linked_list_head_node.next = null			  // reset head.next = null
 		}
 	}
 
