fc04b43dd8b7cef9554fb69d691d1b5c4afb00ae
arslan-ashraf <arslanash@gmail.com>
2025-07-13 11:41:23 -0400

    changes

--- algorithms/dynamic-programming/best-time-to-buy-and-sell-stock-iii.js
+++ algorithms/dynamic-programming/best-time-to-buy-and-sell-stock-iii.js
@@ -1,23 +1,43 @@
 function best_time_to_buy_and_sell_stock_iii(prices, num_transactions = 2){
-	let dp = new Array(num_transactions).fill().map(() => new Array(prices.length))
 
+	let dp = new Array(num_transactions + 1).fill().map(() => new Array(prices.length))
+	for (let i = 0; i < prices.length; i++){ dp[0][i] = 0 }
+	for (let j = 0; j <= num_transactions; j++){ dp[j][0] = 0 }
+
+	let minimum_price = new Array(num_transactions + 1).fill(prices[0])
+
+	for (let i = 1; i < prices.length; i++){
+		for (let k = 1; k <= num_transactions; k++){
+			minimum_price[k] = Math.min(minimum_price[k], prices[i] - dp[k - 1][i - 1])
+			dp[k][i] = Math.max(dp[k][i - 1], prices[i] - minimum_price[k])
+		}
+	}
+
+	console.log(dp)
+	return dp[num_transactions][prices.length - 1]
+	
 }
 
-best_time_to_buy_and_sell_stock_iii([ 3, 3, 5, 0, 0, 3, 1, 4 ])
+console.log(best_time_to_buy_and_sell_stock_iii([ 3, 3, 5, 0, 0, 3, 1, 4 ]) == 6)
 
 
 
 function best_time_to_buy_and_sell_stock_iii_space_optimized(prices, num_transactions = 2){
 	if (prices.length == 0) return 0
+
 	let dp = new Array(num_transactions + 1).fill(0)
+
 	let minimum_price = new Array(num_transactions + 1).fill(prices[0])
+
 	for (let i = 1; i < prices.length; i++){
 		for (let k = 1; k <= num_transactions; k++){
 			minimum_price[k] = Math.min(minimum_price[k], prices[i] - dp[k - 1])
 			dp[k] = Math.max(dp[k], prices[i] - minimum_price[k])
 		}
 	}
+
+	console.log(dp)
 	return dp[num_transactions]
 }
 
-best_time_to_buy_and_sell_stock_iii_space_optimized([ 3, 3, 5, 0, 0, 3, 1, 4 ])
+console.log(best_time_to_buy_and_sell_stock_iii_space_optimized([ 3, 3, 5, 0, 0, 3, 1, 4 ]) == 6)
