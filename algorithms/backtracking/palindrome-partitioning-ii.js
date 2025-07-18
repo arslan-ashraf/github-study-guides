237ec166f627d0ed88f65aba3e67f309bfbf5ffd
arslan-ashraf <arslanash@gmail.com>
2025-07-18 11:51:24 -0400

    changes

--- algorithms/backtracking/palindrome-partitioning-ii.js
+++ algorithms/backtracking/palindrome-partitioning-ii.js
@@ -1,3 +1,20 @@
 function palindrome_partitioning_ii(str){
 	
+	// part 1, calculate the is substring palindromic
+	is_substring_palindrome_dp = new Array(str.length).fill().map(() => new Array(str.length).fill(true))
+	for (let i = str.length - 1; i >= 0; i--){
+		for (let j = i + 1; j < str.length; j++){
+			if (str[i] == str[j]){
+				is_substring_palindrome_dp[i][j] = is_substring_palindrome_dp[i + 1][j - 1]
+			} else {
+				is_substring_palindrome_dp[i][j] = false
+			}
+		}
+	}
+
+	// part 2
+
+	console.log(is_substring_palindrome_dp)
 }
+
+console.log(palindrome_partitioning_ii("xbaabacax"))
