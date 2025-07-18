6cf08d907e0c9dcf387c02e984550f7787d97ecc
arslan-ashraf <arslanash@gmail.com>
2025-07-18 13:19:00 -0400

    changes

--- algorithms/backtracking/palindrome-partitioning-ii.js
+++ algorithms/backtracking/palindrome-partitioning-ii.js
@@ -1,6 +1,7 @@
 function palindrome_partitioning_ii(str){
 	
 	let n = str.length
+	if (n == 0) return 0
 
 	// part 1, calculate the is substring palindromic
 	is_substring_palindrome_dp = new Array(n).fill().map(() => new Array(n).fill(true))
@@ -19,22 +20,26 @@ function palindrome_partitioning_ii(str){
 	let minimum_cut_count = new Array(n)
 	for (let i = 0; i < n; i++){ minimum_cut_count[i] = i }
 
-	for (let i = 1; i < n; i++){
-		for (let j = 0; j <= i; j++){
-			if (is_substring_palindrome_dp[j][i]){
-				if (j == 0){
-					is_substring_palindrome_dp[i] = 0
+	for (let column = 1; column < n; column++){
+		for (let row = 0; row <= column; row++){
+
+			// is substring from row index to column index palindromic
+			if (is_substring_palindrome_dp[row][column]){ 
+				if (row == 0){
+					minimum_cut_count[column] = 0
 				} else {
-					minimum_cut_count[i] = Math.min(minimum_cut_count[i], 
-													minimum_cut_count[j - 1] + 1)
+					minimum_cut_count[column] = Math.min(minimum_cut_count[column], 
+													  	 minimum_cut_count[row - 1] + 1)
 				}
 			}
+
 		}
 	}
 
 	console.log(is_substring_palindrome_dp)
 	console.log(minimum_cut_count)
 	return minimum_cut_count[n - 1]
+
 }
 
 console.log(palindrome_partitioning_ii("xbaabacax"))
