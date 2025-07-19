46e7e30982b2c4df6b67556abeffc0443b08df74
arslan-ashraf <arslanash@gmail.com>
2025-07-19 18:45:22 -0400

    changes

---
+++ algorithms/bit-manipulation/single-number-ii.js
@@ -0,0 +1,5 @@
+function single_number_ii(arr){
+
+}
+
+single_number_ii([3, 4, 4, 1, 3, 4, 3]) // returns 1
--- algorithms/bit-manipulation/single-number-iii.js
+++ algorithms/bit-manipulation/single-number-iii.js
@@ -2,7 +2,7 @@ function single_number_iii(arr){
 	let XOR_of_all_nums = 0
 	for (let i = 0; i < arr.length; i++){ XOR_of_all_nums ^= arr[i] }
 
-	// trick to turn a number's rightmost bit to 1 and all other bits to 0
+	// XOR trick to turn a number's rightmost bit to 1 and all other bits to 0
 	// ex: 10110100 -> 00000100
 	let right_most_bit = XOR_of_all_nums & -XOR_of_all_nums
 
