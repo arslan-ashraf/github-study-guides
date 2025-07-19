3e07150e68092d7013ccd8fd5883b364454136fa
arslan-ashraf <arslanash@gmail.com>
2025-07-19 10:52:16 -0400

    changes

--- algorithms/sorting-problems/merge-intervals.js
+++ algorithms/sorting-problems/merge-intervals.js
@@ -4,11 +4,18 @@ function merge_intervals(intervals){
 	let result = [intervals[0]]
 
 	for(let i = 1; i < intervals.length; i++){
-		if (result[result.length - 1][1] < intervals[i][0]){
-			result.push(intervals[i])
+
+		let last_non_overlapping_interval_end_time = result[result.length - 1][1]
+		let current_interval_start_time = intervals[i][0]
+		let current_interval_end_time = intervals[i][1]
+
+		// is there an overlap
+		if (current_interval_start_time > last_non_overlapping_interval_end_time){
+			result.push(intervals[i]) // no overlap
 		} else {
-			result[result.length - 1][1] = Math.max(result[result.length - 1][1],
-													intervals[i][1])
+			// last_non_overlapping_interval_end_time
+			result[result.length - 1][1] = Math.max(last_non_overlapping_interval_end_time,
+													current_interval_end_time)
 		}
 	}
 	return result
