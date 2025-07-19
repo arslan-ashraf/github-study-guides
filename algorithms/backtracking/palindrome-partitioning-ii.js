eae220a8582e1be4282043662fc426bc774fde3b
arslan-ashraf <arslanash@gmail.com>
2025-07-19 09:27:54 -0400

    changes

--- algorithms/backtracking/palindrome-partitioning-ii.js
+++ algorithms/backtracking/palindrome-partitioning-ii.js
@@ -4,39 +4,47 @@ function palindrome_partitioning_ii(str){
 	if (n == 0) return 0
 
 	// part 1, calculate the is substring palindromic
-	is_substring_palindrome_dp = new Array(n).fill().map(() => new Array(n).fill(true))
+	// we only need the top left, ignore bottom left below the diagonal
+	is_substring_palindrome_cache = new Array(n).fill().map(() => new Array(n).fill(true))
 	
 	for (let i = n - 1; i >= 0; i--){
 		for (let j = i + 1; j < n; j++){
 			if (str[i] == str[j]){
-				is_substring_palindrome_dp[i][j] = is_substring_palindrome_dp[i + 1][j - 1]
+				is_substring_palindrome_cache[i][j] = is_substring_palindrome_cache[i + 1][j - 1]
 			} else {
-				is_substring_palindrome_dp[i][j] = false
+				is_substring_palindrome_cache[i][j] = false
 			}
 		}
 	}
 
-	// part 2
+	// part 2, dynamic programming
 	let minimum_cut_count = new Array(n)
+
 	for (let i = 0; i < n; i++){ minimum_cut_count[i] = i }
 
+	// for each column, we go down the rows up to the current column
+	// as the matrix is meaningless below the diagonal
 	for (let column = 1; column < n; column++){
 		for (let row = 0; row <= column; row++){
 
 			// is substring from row index to column index palindromic
-			if (is_substring_palindrome_dp[row][column]){ 
-				if (row == 0){
-					minimum_cut_count[column] = 0
+			if (is_substring_palindrome_cache[row][column]){ 
+
+				let i = column, j = row  		// for clarity
+
+				if (j == 0){
+					minimum_cut_count[i] = 0
 				} else {
-					minimum_cut_count[column] = Math.min(minimum_cut_count[column], 
-													  	 minimum_cut_count[row - 1] + 1)
+					minimum_cut_count[i] = Math.min(minimum_cut_count[i], 
+													minimum_cut_count[j - 1] + 1)
 				}
+
 			}
 
 		}
 	}
 
-	console.log(is_substring_palindrome_dp)
+	console.log(is_substring_palindrome_cache)
 	console.log(minimum_cut_count)
 	return minimum_cut_count[n - 1]
 
