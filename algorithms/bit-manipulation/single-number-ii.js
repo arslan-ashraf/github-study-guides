9f927405a85824e3ffb98563d0346c664199c79d
arslan-ashraf <arslanash@gmail.com>
2025-07-19 20:28:02 -0400

    changes

--- algorithms/bit-manipulation/single-number-ii.js
+++ algorithms/bit-manipulation/single-number-ii.js
@@ -1,13 +1,18 @@
 function single_number_ii(arr){
-	let summed_bits = new Uint32Array(32).fill(0)
-	for (let i = 31; i >= 0; i--){
+	let summed_bits = new Array(32).fill(0)
+	for (let i = 0; i < 32; i++){
 		for (let j = 0; j < arr.length; j++){
-			console.log(1 << i)
-			summed_bits[i] += arr[j] & (1 << i)
+			// arr[j] & (1 << i) gets the bit at position i
+			if ((arr[j] & (1 << i)) != 0){
+				summed_bits[31 - i] += 1
+			}
 		}
+		summed_bits[31 - i] %= 3
 	}
 
 	console.log(summed_bits)
+	let result_bit_string = summed_bits.join('')
+	return parseInt(result_bit_string, 2)
 }
 
 single_number_ii([3, 4, 4, 1, 3, 4, 3]) // returns 1
