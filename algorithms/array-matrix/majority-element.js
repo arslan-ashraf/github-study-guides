465951068803999c18aed3db286dcc2c3272feac
arslan-ashraf <arslanash@gmail.com>
2025-09-13 16:27:58 -0400

    changes

--- algorithms/array-matrix/majority-element.js
+++ algorithms/array-matrix/majority-element.js
@@ -1,5 +1,5 @@
-// possibly does not pass all test cases
-function find_majority_element(arr){
+// does not pass all test cases
+function find_majority_element1(arr){
 
 	let majority_guess = arr[0]
 	let majority_element = arr[0]
@@ -27,6 +27,28 @@ function find_majority_element(arr){
 	return majority_element
 }
 
+
+
+function find_majority_element(arr){
+	let vote_count = 0
+	let majority_element = null
+
+	for (let i = 0; i < arr.length; i++){
+		if (vote_count = 0){
+			majority_element = arr[i]
+			vote_count = 1
+		} else {
+			if (arr[i] == majority_element){
+				vote_count += 1
+			} else {
+				vote_count -= 1
+			}
+		}
+	}
+	
+	return majority_element
+}
+
 console.log(find_majority_element([1,0,1]) == 1)
 console.log(find_majority_element([1,0,1,0,1,1,0])  == 1)
 console.log(find_majority_element([0,0,1,1,1]) == 1)
@@ -37,9 +59,3 @@ console.log(find_majority_element([1,1,1,1,0,0,0]) == 1)
 console.log(find_majority_element([1,1,1,3,3,3,1,4,1]) == 1)
 console.log(find_majority_element([1,1,1,3,3,3,1,1,4]) == 1)
 console.log(find_majority_element([1,1,1,3,3,3,1]) == 1)
-
-
-
-function find_majority_element1(arr){
-	
-}
