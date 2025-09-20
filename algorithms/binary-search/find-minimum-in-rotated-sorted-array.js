263b1dc5460ed95f7d6498bd89c6af0a104db005
arslan-ashraf <arslanash@gmail.com>
2025-09-20 09:32:52 -0400

    changes

--- algorithms/binary-search/find-minimum-in-rotated-sorted-array.js
+++ algorithms/binary-search/find-minimum-in-rotated-sorted-array.js
@@ -1,4 +1,25 @@
-function find_minimum_rotated_sorted_array(arr) {
+function find_minimum_rotated_sorted_array(arr){
+    let left = 0
+    let right = arr.length - 1
+    let minimum_element = arr[0]
+    while (left <= right){
+        let middle = Math.floor((left + right)/2)
+        minimum_element = Math.min(minimum_element, arr[middle])
+        if (arr[middle] > arr[right]){
+            left = middle + 1
+        } else {
+            right = middle - 1
+        }
+    }
+    return minimum_element
+}
+
+console.log(find_minimum_rotated_sorted_array([4, 5, 6, 7, 8, 9, 0, 1, 2, 3]) == 0)
+console.log(find_minimum_rotated_sorted_array([3, 4, 5, 1, 2]) == 1)
+console.log(find_minimum_rotated_sorted_array([3, 1, 2]) == 1)
+
+
+function find_minimum_rotated_sorted_array1(arr) {
     let left = 0
     let right = arr.length - 1
     while (left < right){
@@ -12,4 +33,5 @@ function find_minimum_rotated_sorted_array(arr) {
     return arr[left]
 }
 
-console.log(find_minimum_rotated_sorted_array([4, 5, 6, 7, 8, 9, 0, 1, 2, 3]) == 0)
+console.log(find_minimum_rotated_sorted_array1([4, 5, 6, 7, 8, 9, 0, 1, 2, 3]) == 0)
+console.log(find_minimum_rotated_sorted_array1([3, 1, 2]) == 1)
