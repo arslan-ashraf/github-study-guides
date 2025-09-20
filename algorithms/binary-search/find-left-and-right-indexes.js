07252e6fa3f5e7cc73d69d735b05890fd9cdc154
arslan-ashraf <arslanash@gmail.com>
2025-09-20 10:12:35 -0400

    changes

--- algorithms/binary-search/find-left-and-right-indexes.js
+++ algorithms/binary-search/find-left-and-right-indexes.js
@@ -28,7 +28,7 @@ function find_extreme_index(arr, target, search_side){
     return extreme_index
 }
 
-// function search_sorted_range(arr, target) {
+// function find_left_and_right_indexes(arr, target) {
 //     let left_index = find_left_index(arr, target)
 //     let right_index = find_right_index(arr, target)
 //     return [left_index, right_index]
@@ -73,13 +73,14 @@ function find_extreme_index(arr, target, search_side){
 
 let test1 = [1]
 let test2 = [5,7,7,8,8,10]
-console.log(search_sorted_range(test1, 1))
-console.log(search_sorted_range(test2, 8))
+console.log(find_left_and_right_indexes(test1, 1))
+console.log(find_left_and_right_indexes(test2, 8))
 
 let arr1 = [0,1,2,3,4,5,6,7,8,9,10]
 let arr2 = [1,5,5,5,5,5,5,5,5,9,10]
 let arr3 = [0,1,2,3,4,5,5,5,6,7,10]
 
-console.log(search_sorted_range(arr1, 1))
-console.log(search_sorted_range(arr2, 5))
-console.log(search_sorted_range(arr3, 5))
+console.log(find_left_and_right_indexes(arr1, 1))
+console.log(find_left_and_right_indexes(arr2, 5))
+console.log(find_left_and_right_indexes(arr2, 7))
+console.log(find_left_and_right_indexes(arr3, 5))
