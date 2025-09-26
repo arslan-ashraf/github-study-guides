3ac59dd4ffdddf40b2aa32c4e4c4b7a7930da423
arslan-ashraf <arslanash@gmail.com>
2025-09-26 14:55:07 -0400

    changes

--- algorithms/bit-manipulation/exponential-power-function.js
+++ algorithms/bit-manipulation/exponential-power-function.js
@@ -1,23 +1,3 @@
-function exponential_power(x, n){
-	let result = 1
-	let absolute_n = Math.abs(n)
-	while (absolute_n != 0){
-		if ((absolute_n & 1) == 1){
-			result = result * x
-		}
-		x = x * x
-		absolute_n = absolute_n >>> 1
-	}
-	return n > 0 ? result : 1/result
-}
-
-
-console.log(exponential_power(2, 10) == 1024)
-console.log(exponential_power(3, -2) == 1/9)
-console.log(exponential_power(3, 3) == 27)
-console.log(exponential_power(3, 4) == 81)
-
-
 function exponential_power_recursive(x, n){
 	let absolute_n = Math.abs(n)
 	let result = backtrack(x, absolute_n)
@@ -39,3 +19,23 @@ console.log(exponential_power_recursive(2, 10) == 1024)
 console.log(exponential_power_recursive(3, -2) == 1/9)
 console.log(exponential_power_recursive(3, 3) == 27)
 console.log(exponential_power_recursive(3, 4) == 81)
+
+
+function exponential_power(x, n){
+	let result = 1
+	let absolute_n = Math.abs(n)
+	while (absolute_n != 0){
+		if ((absolute_n & 1) == 1){
+			result = result * x
+		}
+		x = x * x
+		absolute_n = absolute_n >>> 1
+	}
+	return n > 0 ? result : 1/result
+}
+
+
+console.log(exponential_power(2, 10) == 1024)
+console.log(exponential_power(3, -2) == 1/9)
+console.log(exponential_power(3, 3) == 27)
+console.log(exponential_power(3, 4) == 81)
