1a565da7d49349c6dc8db374291db2b80c1e1f91
arslan-ashraf <arslanash@gmail.com>
2025-09-26 14:54:43 -0400

    changes

--- algorithms/bit-manipulation/exponential-power-function.js
+++ algorithms/bit-manipulation/exponential-power-function.js
@@ -20,7 +20,22 @@ console.log(exponential_power(3, 4) == 81)
 
 function exponential_power_recursive(x, n){
 	let absolute_n = Math.abs(n)
-	let result = backtrack(x, n)
+	let result = backtrack(x, absolute_n)
 	return n > 0 ? result : 1/result
 }
 
+function backtrack(x, n){
+	if (n == 0) return 1
+	if (n == 1) return x
+
+	if (n % 2 == 1){
+		return x * backtrack(x, (n - 1)/2) * backtrack(x, (n - 1)/2)
+	} else {
+		return backtrack(x, n/2) * backtrack(x, n/2)
+	}
+}
+
+console.log(exponential_power_recursive(2, 10) == 1024)
+console.log(exponential_power_recursive(3, -2) == 1/9)
+console.log(exponential_power_recursive(3, 3) == 27)
+console.log(exponential_power_recursive(3, 4) == 81)
