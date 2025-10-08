05bd43a50c3eb1fb5fadd1b7d88f3fcee60134d9
arslan-ashraf <arslanash@gmail.com>
2025-10-08 16:32:53 -0400

    changes

--- algorithms/tree-problems/smallest-string-starting-from-leaf.js
+++ algorithms/tree-problems/smallest-string-starting-from-leaf.js
@@ -9,25 +9,31 @@ function TreeNode(value){
 	this.next = null
 }
 
-let one = new TreeNode(1)
-let four = new TreeNode(4)
+let twenty_five = new TreeNode(25)
+let twenty_four = new TreeNode(24)
+let twenty_three = new TreeNode(23)
 let five = new TreeNode(5)
 let six = new TreeNode(6)
-let seven = new TreeNode(7)
-let eight = new TreeNode(8)
-let nine = new TreeNode(9)
+let four = new TreeNode(4)
+let one = new TreeNode(1)
+let zero_first = new TreeNode(0)
+let zero_second = new TreeNode(0)
 
-seven.left = five
-seven.right = nine
 
-five.left = four
-five.right = six
+twenty_five.left = twenty_four
+twenty_five.right = six
 
-four.left = one
+twenty_four.left = twenty_three
+twenty_four.right = five
 
-nine.left = eight
+six.left = four
+six.right = one
 
-//					7
-//			5				9
-//		4		6		8
-//	 1
+five.left = zero_first
+
+one.left = zero_second
+
+//					25
+//			24				6
+//		23		5		4		1
+//	 		  0				  0
